add6a57cbb3668684de13be0610d10e71b7d1354
seesaw <seesaw.schindele@gmail.com>
2018-04-19 16:21:15 -0700

    re-add missing styling for nav bar

--- ListApplication/Screens/screenStyles.js
+++ ListApplication/Screens/screenStyles.js
@@ -41,6 +41,39 @@ export default StyleSheet.create({
    spacer: {
      width: 60,
    },
+   // listNavBar styles
+   listNavBarContainer: {
+      height: 60,
+      backgroundColor: '#424242',
+   },
+   listNavBarLogoutOpacity: {
+      position: 'absolute',
+      top: 8,
+      left: 10,
+   },
+   listNavBarOptionsOpacity: {
+      position: 'absolute',
+      top: 6,
+      right: 80,
+   },
+   listNavBarNewListButtonOpacity: {
+      width: 48,
+      height: 48,
+      position: 'absolute',
+      top: 8,
+      right: 0,
+   },
+   listNavBarNewListButton: {
+      fontSize: 48,
+      color: "#ffffff",
+   },
+   listNavBarLogo: {
+      width: 36,
+      height: 36,
+      position: 'absolute',
+      left: 200,
+      top: 10,
+   },
    // listSelector styles
    listSelectorContainer: {
      flex: 1,
